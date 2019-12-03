9eecd41731836bb0898f73382163bd9fcfb66dcb
Jevon <kurisaq@gmail.com>
2019-12-03 12:25:46 +0700

    Add project files.

---
+++ ReLearning1/Pages/ListGadget/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ReLearning1.Model;
+
+namespace ReLearning1.Pages.ListGadget
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public  IEnumerable<Gadget> Gadgets { get; set; }
+
+
+
+        public async Task OnGet()
+        {
+            Gadgets = await _db.Gadget.ToListAsync();    
+        }
+
+
+    }
+}
